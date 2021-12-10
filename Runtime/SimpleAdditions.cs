14cd385f59172146704455e5ce53ac0557138553
Sam <sammckay@btinternet.com>
2021-12-10 13:00:31 +0000

    Re init package

---
+++ Runtime/SimpleAdditions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PackageTester.Runtime
+{
+    public static class SimpleAdditions
+    {
+        public static int AddIntegers(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
